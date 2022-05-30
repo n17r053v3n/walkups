fc4665526c942532993a97f9024a40faec6c792c
n17r053v3n <n17r053v3n@protonmail.com>
2022-05-30 11:57:20 +0200

    added upload buttons and some initial upload functionality

---
+++ script.js
@@ -0,0 +1,14 @@
+// store a reference to our file handle
+let fileHandle;
+
+async function getFile() {
+  // open file picker
+  [fileHandle] = await window.showOpenFilePicker();
+
+  if (fileHandle.kind === 'file') {
+    // run file code
+  } else if (fileHandle.kind === 'directory') {
+    // run directory code
+  }
+
+}
