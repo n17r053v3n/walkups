c192a0c51efa8d6c27e6cc3c5e2be2da1694b302
n17r053v3n <n17r053v3n@protonmail.com>
2022-06-01 23:30:55 +0200

    fuck the API gonna do a bad workaround

---
+++ songs/lineup.js
@@ -0,0 +1,10 @@
+[{"PlayerOne":"One.mp3",
+"PlayerTwo":"Two.mp3",
+"PlayerThree":"Three.mp3",
+"PlayerFour":"Four.mp3",
+"PlayerFive":"Five.mp3",
+"PlayerSix":"Six.mp3",
+"PlayerSeven":"Seven.mp3",
+"PlayerEight":"Eight.mp3",
+"PlayerNine":"Nine.mp3",
+}]
